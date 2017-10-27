26a36b1662fb57810e8111c15f8d6924d311c031
QNattaponQ <sunshiro9@gmail.com>
2017-10-27 15:52:59 +0700

    เทสๆๆๆ

--- src/Test2.go
+++ src/Test2.go
@@ -2,31 +2,69 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"gopkg.in/mux"
 	"gopkg.in/mgo.v2"
+	"time"
+	"regexp"
+	//"gopkg.in/mgo.v2/bson"
+	"strings"
 	"gopkg.in/mgo.v2/bson"
+	"math/rand"
+	"fmt"
+
 )
 
-func ErrorWithJSON(w http.ResponseWriter, message string, code int) {
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	w.WriteHeader(code)
-	fmt.Fprintf(w, "{message: %q}", message)
-}
+func ErrorWithJSON(w http.ResponseWriter, json []byte, code int) {
+	w.Header().Set("x-request-id", newUUID())
+	w.Header().Set("datetime", time.Now().Format("2006-01-02 15:04:05+0700"))
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("x-roundtrip", "")
+	w.Header().Set("x-job-id", "")
 
-func ResponseWithJSON(w http.ResponseWriter, json []byte, code int) {
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	w.WriteHeader(code)
+	//w.WriteHeader(code)
+	//fmt.Fprintf(w, "{message: %q}", message)
 	w.Write(json)
 }
 
-type Book struct {
-	ISBN    string   `json:"isbn"`
-	Title   string   `json:"title"`
-	Authors []string `json:"authors"`
-	Price   string   `json:"price"`
+func ResponseWithJSON(w http.ResponseWriter, json []byte, code int) {
+	w.Header().Set("x-request-id", newUUID())
+	w.Header().Set("datetime", time.Now().Format("2006-01-02 15:04:05+0700"))
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("x-roundtrip", "")
+	w.Header().Set("x-job-id", "")
+	//w.WriteHeader(code)
+	//w.Write(json)
+	w.Write(json)
+}
+
+type WalletAccount struct {
+	ID        bson.ObjectId `bson:"_id,omitempty"`
+	CitizenID     int     `json:"citizen_id" bson:"citizen_id"`
+	FullName      string  `json:"full_name" bson:"full_Name"`
+	WalletID      int     `json:"wallet_id" bson:"wallet_id"`
+	OpenDateTime  string  `json:"open_datetime" bson:"open_datetime"`
+	LedgerBalance float32 `json:"ledger_balance" bson:"ledger_balance"`
+}
+
+type MsgBody struct {
+	RsBody RsBody `json:"rsBody"`
+	Error Error `json:"error"`
+}
+
+type RsBody struct {
+	WalletID	int		`json:"wallet_id"`
+	OpenDateTime  string  `json:"open_datetime"`
+}
+
+type Error struct {
+	ErCode string 	`bson:"error code"`
+	ErDesc string	`bson:"error description"`
+}
+
+type ErrorList struct {
+	Error Error
 }
 
 func main() {
@@ -40,111 +78,127 @@ func main() {
 	ensureIndex(session)
 
 	mux := mux.NewRouter()
-	mux.HandleFunc("/WalletAccounts", allBooks(session)).Methods("GET")
-	mux.HandleFunc("/CreateWalletAccount", addBook(session)).Methods("POST")
+	//mux.HandleFunc("/v1/accounts/{wallet_id}", getAccountByWalletID(session)).Methods("GET")
+	mux.HandleFunc("/v1/accounts", createWallets(session)).Methods("POST")
 	//http.ListenAndServe("localhost:5000", mux)
-	log.Fatal(http.ListenAndServe("localhost:2222", mux))
+	log.Fatal(http.ListenAndServe("localhost:3333", mux))
 }
 
 func ensureIndex(s *mgo.Session) {
 	session := s.Copy()
 	defer session.Close()
 
-	c := session.DB("store").C("books")
+	c := session.DB("wallets").C("accounts")
 
 	index := mgo.Index{
-		Key:        []string{"isbn"},
-		Unique:     true,
-		DropDups:   true,
-		Background: true,
-		Sparse:     true,
+		Key:		[]string{"citizen_id"},
+		Unique:		true,
+		DropDups:	true,
+		Background:	true,
+		Sparse:		true,
 	}
+
 	err := c.EnsureIndex(index)
 	if err != nil {
 		panic(err)
 	}
 }
 
-func allBooks(s *mgo.Session) func(w http.ResponseWriter, r *http.Request) {
+func createWallets(s *mgo.Session) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		session := s.Copy()
 		defer session.Close()
 
-		c := session.DB("store").C("books")
+		var accounts WalletAccount
+		var error Error
 
-		var books []Book
-		err := c.Find(bson.M{}).All(&books)
-		if err != nil {
-			ErrorWithJSON(w, "Database error", http.StatusInternalServerError)
-			log.Println("Failed get all books: ", err)
-			return
-		}
-
-		respBody, err := json.MarshalIndent(books, "", "  ")
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		ResponseWithJSON(w, respBody, http.StatusOK)
-	}
-}
-
-func addBook(s *mgo.Session) func(w http.ResponseWriter, r *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		session := s.Copy()
-		defer session.Close()
-
-		var book Book
 		decoder := json.NewDecoder(r.Body)
-		err := decoder.Decode(&book)
+		err := decoder.Decode(&accounts)
 		if err != nil {
-			ErrorWithJSON(w, "Incorrect body", http.StatusBadRequest)
+			//error = append(
+
 			return
 		}
 
-		c := session.DB("store").C("books")
+		if (!IsLetter(accounts.FullName)) || (!Len(accounts.FullName)) {
+			ErrorWithJSON(w, "Incorrect Full Name", http.StatusBadRequest)
+			return
+		}
 
-		err = c.Insert(book)
+		if LenCitizenId(accounts.CitizenID) {
+			ErrorWithJSON(w, "Incorrect Citizen ID", http.StatusBadRequest)
+			return
+		}
+
+		accounts.FullName=strings.ToUpper(accounts.FullName)
+		accounts.WalletID=1234567890
+		accounts.OpenDateTime = time.Now().Format("2006-01-02 15:04:05+0700")
+		accounts.LedgerBalance = 0.00
+
+		c := session.DB("wallets").C("accounts")
+
+		err = c.Insert(accounts)
 		if err != nil {
 			if mgo.IsDup(err) {
-				ErrorWithJSON(w, "Book with this ISBN already exists", http.StatusBadRequest)
+				ErrorWithJSON(w, "Accounts with this Citizen ID already exists", http.StatusBadRequest)
 				return
 			}
 
 			ErrorWithJSON(w, "Database error", http.StatusInternalServerError)
-			log.Println("Failed insert book: ", err)
+			log.Println("Failed insert accounts: ", err)
 			return
 		}
 
-		w.Header().Set("Content-Type", "application/json")
-		w.Header().Set("Location", r.URL.Path+"/"+book.ISBN)
-		w.WriteHeader(http.StatusCreated)
+		respbody := RsBody{
+			OpenDateTime:accounts.OpenDateTime,
+			WalletID:accounts.WalletID,
+		}
+		error := Error{
+			ErCode:"",
+			ErDesc:"",
+		}
+
+		msgbody :=MsgBody{
+			respbody,
+			error,
+		}
+
+		fmt.Println(msgbody)
+		respBody, err := json.MarshalIndent(msgbody, "", "  ")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		ResponseWithJSON(w, respBody, http.StatusCreated)
+
 	}
 }
+
 /*
-func bookByISBN(s *mgo.Session) func(w http.ResponseWriter, r *http.Request) {
+func getAccountByWalletID(s *mgo.Session) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		session := s.Copy()
 		defer session.Close()
 
-		isbn := pat.Param(r, "isbn")
+		vars := mux.Vars(r)
+		wallets := vars["wallet_id"]
 
-		c := session.DB("store").C("books")
+		c := session.DB("wallets").C("accounts")
 
-		var book Book
-		err := c.Find(bson.M{"isbn": isbn}).One(&book)
+		var accounts WalletAccount
+		err := c.Find(bson.M{"wallet_id": wallets}).One(&accounts)
 		if err != nil {
 			ErrorWithJSON(w, "Database error", http.StatusInternalServerError)
-			log.Println("Failed find book: ", err)
+			log.Println("Failed find wallet_id: ", err)
 			return
 		}
 
-		if book.ISBN == "" {
+		if accounts.WalletID == nil {
 			ErrorWithJSON(w, "Book not found", http.StatusNotFound)
 			return
 		}
 
-		respBody, err := json.MarshalIndent(book, "", "  ")
+		respBody, err := json.MarshalIndent(accounts, "", "  ")
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -152,64 +206,44 @@ func bookByISBN(s *mgo.Session) func(w http.ResponseWriter, r *http.Request) {
 		ResponseWithJSON(w, respBody, http.StatusOK)
 	}
 }
+*/
+var IsLetter = regexp.MustCompile(`^[a-zA-Z.,-]+( [a-zA-Z.,-]+)+$`).MatchString
 
-func updateBook(s *mgo.Session) func(w http.ResponseWriter, r *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		session := s.Copy()
-		defer session.Close()
-
-		isbn := pat.Param(r, "isbn")
-
-		var book Book
-		decoder := json.NewDecoder(r.Body)
-		err := decoder.Decode(&book)
-		if err != nil {
-			ErrorWithJSON(w, "Incorrect body", http.StatusBadRequest)
-			return
-		}
-
-		c := session.DB("store").C("books")
-
-		err = c.Update(bson.M{"isbn": isbn}, &book)
-		if err != nil {
-			switch err {
-			default:
-				ErrorWithJSON(w, "Database error", http.StatusInternalServerError)
-				log.Println("Failed update book: ", err)
-				return
-			case mgo.ErrNotFound:
-				ErrorWithJSON(w, "Book not found", http.StatusNotFound)
-				return
-			}
-		}
-
-		w.WriteHeader(http.StatusNoContent)
+func Len(s string) bool {
+	if len(s)<=50 {
+		return true
 	}
+	return false
 }
 
-func deleteBook(s *mgo.Session) func(w http.ResponseWriter, r *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		session := s.Copy()
-		defer session.Close()
-
-		isbn := pat.Param(r, "isbn")
-
-		c := session.DB("store").C("books")
-
-		err := c.Remove(bson.M{"isbn": isbn})
-		if err != nil {
-			switch err {
-			default:
-				ErrorWithJSON(w, "Database error", http.StatusInternalServerError)
-				log.Println("Failed delete book: ", err)
-				return
-			case mgo.ErrNotFound:
-				ErrorWithJSON(w, "Book not found", http.StatusNotFound)
-				return
-			}
-		}
-
-		w.WriteHeader(http.StatusNoContent)
+func LenCitizenId(i int) bool {
+	if len(string(i))==13 {
+		return true
 	}
-*/
+	return false
+}
+
+var walletId = randInt(0,9999999999)
+
+func newUUID() (string) {
+	uuid := make([]byte, 16)
+	//n, err := io.ReadFull(rand.Reader, uuid)
+	//if n != len(uuid) || err != nil {
+	//	return "", err
+	//}
+	// variant bits; see section 4.1.1
+	uuid[8] = uuid[8]&^0xc0 | 0x80
+	// version 4 (pseudo-random); see section 4.1.3
+	uuid[6] = uuid[6]&^0xf0 | 0x40
+	return fmt.Sprintf("%x-%x-%x-%x-%x", uuid[0:4], uuid[4:6], uuid[6:8], uuid[8:10], uuid[10:])
+}
+
+func randInt(min int, max int) int {
+	return min + rand.Intn(max-min)
+}
+
+
+
+
+
 
